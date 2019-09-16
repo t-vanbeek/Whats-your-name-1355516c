11b6091a14db902f000006a99503d79a441ad858
Thorvald van Beek <54794887+t-vanbeek@users.noreply.github.com>
2019-09-16 14:09:47 +0200

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,5 @@
+<?php
+$voornaam = readline("> Wat is je voornaam? \n "); PHP_EOL;
+$achternaam = readline("> Wat is je achternaam? \n a");PHP_EOL;
+
+echo '> Jouw naam is: '.$voornaam; echo " " .$achternaam;
